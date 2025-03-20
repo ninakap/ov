9aa1ae398e6ab41afe4dee12450fcfe03fb9f974
ninakap <1214412@student.roc-nijmegen.nl>
2025-03-20 12:54:37 +0100

    push

---
+++ src/ovInchecken/AnoniemeChipkaart.java
@@ -0,0 +1,23 @@
+package ovInchecken;
+
+public class AnoniemeChipkaart {
+	    boolean ingecheckt;
+	    double instaptarief;
+	    
+	    public AnoniemeChipkaart(double instaptarief) {
+	        this.ingecheckt = false;
+	        this.instaptarief = instaptarief;
+	    }
+
+		public void inchecken() {
+			// TODO Auto-generated method stub
+		      if (!ingecheckt) {
+		            ingecheckt = true;
+		            System.out.println("Inchecken gelukt.");
+		        } else {
+		            System.out.println("U bent al ingecheckt!");
+		        }
+		    }
+
+		}
+	
---
+++ src/ovInchecken/OplaadPaal.java
@@ -0,0 +1,14 @@
+package ovInchecken;
+
+public class OplaadPaal extends AnoniemeChipkaart {
+
+	public class Main {
+	    public static void main(String[] args) {
+	    	AnoniemeChipkaart kaart = new AnoniemeChipkaart();
+	        kaart.inchecken();  
+	        kaart.inchecken(); 
+
+	    }
+	}
+}  
+

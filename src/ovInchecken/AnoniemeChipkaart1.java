86f8e440b3ff0c3ba27e90856c566f4d20acf3ea
ninakap <1214412@student.roc-nijmegen.nl>
2025-03-27 13:42:50 +0100

    push

--- src/ovInchecken/AnoniemeChipkaart1.java
+++ src/ovInchecken/AnoniemeChipkaart1.java
@@ -3,12 +3,15 @@ package ovInchecken;
 public class AnoniemeChipkaart1 extends OplaadPaal {
 
 	public class Main {
-	    public static void main(String[] args) {
-	    	OplaadPaal kaart = new OplaadPaal();
-	        kaart.inchecken();  
-	        kaart.inchecken(); 
+	    public static void main(String[] args) {{
+	            OplaadPaal kaart = new OplaadPaal(5, 10);
 
+	            kaart.inchecken(); // Probeer in te checken (moet mislukken)
+	            kaart.opladen(10); // Laad €10 op
+	            kaart.inchecken(); // Probeer opnieuw in te checken 
+	        }
+	    
 	    }
 	}
-}  
+  
 

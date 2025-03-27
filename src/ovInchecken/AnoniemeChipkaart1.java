7de5651d079c7d86526a95641e51914384ae9aee
ninakap <1214412@student.roc-nijmegen.nl>
2025-03-27 13:29:42 +0100

    push

---
+++ src/ovInchecken/AnoniemeChipkaart1.java
@@ -0,0 +1,14 @@
+package ovInchecken;
+
+public class AnoniemeChipkaart1 extends OplaadPaal {
+
+	public class Main {
+	    public static void main(String[] args) {
+	    	OplaadPaal kaart = new OplaadPaal();
+	        kaart.inchecken();  
+	        kaart.inchecken(); 
+
+	    }
+	}
+}  
+
--- src/ovInchecken/OplaadPaal.java
+++ src/ovInchecken/OplaadPaal.java
@@ -1,14 +1,32 @@
 package ovInchecken;
 
-public class OplaadPaal extends AnoniemeChipkaart {
+public class OplaadPaal {
+	boolean ingecheckt;
+	double instaptarief;
+	double saldo;
 
-	public class Main {
-	    public static void main(String[] args) {
-	    	AnoniemeChipkaart kaart = new AnoniemeChipkaart();
-	        kaart.inchecken();  
-	        kaart.inchecken(); 
-
-	    }
+	public OplaadPaal(double saldo, double instaptarief) {
+		this.ingecheckt = false;
+		this.saldo = saldo;
+		this.instaptarief = instaptarief;
 	}
-}  
 
+	public void inchecken() {
+		if (ingecheckt) {
+			System.out.println("U bent al ingecheckt!");
+		} else if (saldo >= instaptarief) {
+			saldo -= instaptarief;
+			ingecheckt = true;
+			System.out.println("Inchecken gelukt. nieuw saldo: " + saldo);
+		} else {
+			System.out.println("Niet genoeg saldo om in te checken.");
+		}
+	}
+
+
+
+	public void opladen(double bedrag) {
+		saldo += bedrag;
+		System.out.println("Saldo opgewaardeerd! Nieuw saldo: " + saldo);
+	}
+}

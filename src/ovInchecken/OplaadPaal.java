495a7c8789528294a1271973e6c49ee67b79afdc
ninakap <1214412@student.roc-nijmegen.nl>
2025-04-07 12:07:56 +0200

    psh

--- src/ovInchecken/OplaadPaal.java
+++ src/ovInchecken/OplaadPaal.java
@@ -4,39 +4,40 @@ public class OplaadPaal {
 	boolean ingecheckt;
 	double instaptarief;
 	double saldo;
+	String locatie;
 
 	public OplaadPaal(double saldo, double instaptarief) {
 		this.ingecheckt = false;
 		this.saldo = saldo;
 		this.instaptarief = instaptarief;
+		this.locatie = "";
 	}
 
-	public void inchecken() {
+	public void inchecken(String station) {
 		if (ingecheckt) {
-			System.out.println("U bent al ingecheckt!");
+			System.out.println("U bent al ingecheckt op station: " + locatie);
 		} else if (saldo >= instaptarief) {
 			saldo -= instaptarief;
 			ingecheckt = true;
-			System.out.println("Inchecken gelukt. nieuw saldo: " + saldo);
+			locatie = station;
+			System.out.println("Inchecken gelukt bij " + station + ". Nieuw saldo: " + saldo);
 		} else {
 			System.out.println("Niet genoeg saldo om in te checken.");
 		}
 	}
 
-	public void opladen(double bedrag) {
-		saldo += bedrag;
-		System.out.println("Saldo opgewaardeerd! Nieuw saldo: " + saldo);
-	}
-}
 
-    class Station {
-	String naam;
-	double Breedtegraad;
-	double Lengtegraad;
-
-	public Station(String naam, double latitude, double longitude) {
-		this.naam = naam;
-		this.Breedtegraad = Breedtegraad;
-		this.Lengtegraad = Lengtegraad;
+	public void uitchecken(double ritPrijs, String station) {
+		if (!ingecheckt) {
+			System.out.println("U bent nog niet ingecheckt.");
+		} else if (saldo >= ritPrijs) {
+			saldo -= ritPrijs;
+			System.out.println("Uitgecheckt bij " + station + ". Ritprijs: " + ritPrijs);
+			System.out.println("Nieuw saldo: " + saldo);
+			ingecheckt = false;
+			locatie = "";
+		} else {
+			System.out.println("Niet genoeg saldo om uit te checken.");
+		}
 	}
 }
